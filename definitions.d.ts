1b87929406e32864f596c174f8fb29be035fcf34
Hazem Abdennadher <Hazem0Abdennadher@gmail.com>
2023-07-19 12:08:53 +0100

    fix

--- definitions.d.ts
+++ definitions.d.ts
@@ -2,3 +2,8 @@ declare module '*.glb' {
   const url: string;
   export default url;
 }
+
+declare module '*.wav' {
+  const url: string;
+  export default url;
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,51 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nodePolyfills = require('rollup-plugin-polyfill-node');
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+        },
+      },
+    });
+    // wav files
+    config.module.rules.push({
+      test: /\.wav$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+        },
+      },
+    });
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+        },
+      },
+    });
 
-module.exports = nextConfig
+    // fs
+
+    config.module.rules.push({
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
